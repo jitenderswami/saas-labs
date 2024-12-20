3b1b909e7e8f9785cbe7d983a6134ab7888cdf22
jitenderswami <jitenderswami68017@gmail.com>
2024-12-20 17:06:33 +0530

    Mobile responsive

--- src/components/KickStarterProjectsInfo/KickStarterProjectInfoView.tsx
+++ src/components/KickStarterProjectsInfo/KickStarterProjectInfoView.tsx
@@ -14,41 +14,57 @@ const KickStarterProjectInfoView:React.FC<KickStarterProjectInfoViewProps> = ({ 
     <div className='kickstarter-project-info-view'>
         <h1 className="title">KickStarter Projects</h1>
         
-        {/* Projects Table */}
         <div className="table-container">
             <table className="projects-table">
                 <thead>
                     <tr>
                         <th className="column-header">S.No.</th>
                         <th className="column-header">Title</th>
-                        <th className="column-header">Percentage Funded</th>
-                        <th className="column-header">Amount Pledged</th>
+                        <th className="column-header">Funding</th>
+                        <th className="column-header">Pledged</th>
                     </tr>
                 </thead>
                 <tbody>
                     {projects?.map((project) => (
                         <tr key={project.sNo}>
-                            <td className="serial-number">{project.sNo}</td>
-                            <td className="project-title">{project.title}</td>
-                            <td>
-                                <div className="funding-progress">
-                                    <div 
-                                        className="progress-bar" 
-                                        style={{ width: `${Math.min(project.percentageFunded, 100)}%` }}
-                                    />
-                                    <span className={project.percentageFunded < 55 ? 'percentage-text-dark' : 'percentage-text-light'}>
-                                        {project.percentageFunded}%
-                                    </span>
+                            <td className="serial-number">
+                                <div className="mobile-flex">
+                                    <span className="mobile-label">S.No:</span>
+                                    {project.sNo}
                                 </div>
                             </td>
-                            <td className="amount">{project.amtPledged} {project.currency}</td>
+                            <td className="project-title">
+                                <div className="mobile-flex">
+                                    <span className="mobile-label">Title:</span>
+                                    {project.title}
+                                </div>
+                            </td>
+                            <td>
+                                <div className="mobile-flex">
+                                    <span className="mobile-label">Funding:</span>
+                                    <div className="funding-progress">
+                                        <div 
+                                            className="progress-bar" 
+                                            style={{ width: `${Math.min(project.percentageFunded, 100)}%` }}
+                                        />
+                                        <span className={project.percentageFunded < 55 ? 'percentage-text-dark' : 'percentage-text-light'}>
+                                            {project.percentageFunded}%
+                                        </span>
+                                    </div>
+                                </div>
+                            </td>
+                            <td className="amount">
+                                <div className="mobile-flex">
+                                    <span className="mobile-label">Pledged:</span>
+                                    {project.amtPledged} {project.currency}
+                                </div>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
         </div>
 
-        
         <div className="pagination-controls">
             <button 
                 className="pagination-button"
